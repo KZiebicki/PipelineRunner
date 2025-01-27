e5f2be46acd3b32d83b6f240caf6bd92158df8d5
Kasper Ziębicki <kasper.ziebicki@taskscape.com>
2025-01-27 16:03:38 +0100

    Dodaj pliki projektów.

---
+++ Config.cs
@@ -0,0 +1,15 @@
+﻿namespace PipelineRunner
+{
+    public class Config
+    {
+        public required string WatchDirectory { get; set; }
+        public required string FileSearchPattern { get; set; }
+        public required string CommandsFile { get; set; }
+        public int CycleTimeSeconds { get; set; }
+        public string LogDirectory { get; set; } = "/logs";
+        public string? UseLineFilterPrefix { get; set; }
+        public int ProcessTimeoutSeconds { get; set; }
+        public string? MinimumLogLevel { get; set; }
+        public bool ContinueOnError { get; set; } = false;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System.Text.Json;
+using PipelineRunner;
+using Serilog;
+
+class Program
+{
+    static async Task Main()
+    {
+        Config config = LoadConfig();
+        ConfigureLogger(config.LogDirectory, config.MinimumLogLevel);
+
+        Log.Information("File processing service started.");
+
+        while (true)
+        {
+            try
+            {
+                string[] files = Directory.GetFiles(config.WatchDirectory, config.FileSearchPattern);
+                FileProcessor processor = new(config);
+                List<Task> tasks = [];
+                foreach (var file in files)
+                {
+                    tasks.Add(processor.ProcessFile(file));
+                }
+
+                await Task.WhenAll(tasks);
+
+                Log.Information("Waiting {CycleTime} seconds before the next cycle.", config.CycleTimeSeconds);
+                await Task.Delay(config.CycleTimeSeconds * 1000);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Error in the main application loop.");
+            }
+        }
+    }
+
+    static Config LoadConfig()
+    {
+        string json = File.ReadAllText("appsettings.json");
+        return JsonSerializer.Deserialize<Config>(json) ?? throw new Exception("Failed to load configuration.");
+    }
+
+    static void ConfigureLogger(string logDirectory, string? MinimumLevel = null)
+    {
+        Directory.CreateDirectory(logDirectory);
+
+        var loggerConfig = new LoggerConfiguration();
+
+        switch (MinimumLevel?.ToLower())
+        {
+            case "verbose":
+                loggerConfig.MinimumLevel.Verbose();
+                break;
+            case "debug":
+                loggerConfig.MinimumLevel.Debug();
+                break;
+            case "information":
+                loggerConfig.MinimumLevel.Information();
+                break;
+            case "warning":
+                loggerConfig.MinimumLevel.Warning();
+                break;
+            case "error":
+                loggerConfig.MinimumLevel.Error();
+                break;
+            default:
+                loggerConfig.MinimumLevel.Information();
+                break;
+        }
+
+        loggerConfig.WriteTo.Console();
+        loggerConfig.WriteTo.File(Path.Combine(logDirectory, "log-.txt"), rollingInterval: RollingInterval.Day);
+        Log.Logger = loggerConfig.CreateLogger();
+    }
+}
